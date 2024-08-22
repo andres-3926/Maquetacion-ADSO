17ab235e0c7af66e31d6adad1f592d4f0557c270
crisdalithb <crisdalithb@gmail.com>
2024-08-21 19:18:47 -0500

    Admin user y facturacion

--- scripts.js/reg_compra.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('recibo').value = Math.floor(Math.random() * 1000000);
-
-var today = new Date();
-var date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
-document.getElementById('fecha').value = date;
